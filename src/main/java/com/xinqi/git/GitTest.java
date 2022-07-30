aeb04d93324870d7d8851d8a5c081194aad2fe47
xinqi <xinqi@qq.com>
2022-07-31 06:01:16 +0800

    push commit1

--- src/main/java/com/xinqi/git/GitTest.java
+++ src/main/java/com/xinqi/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello i am hot-fix branch");
         System.out.println("master 4:50 change");
         System.out.println("hot-fix 4:49 change");
+        System.out.println("push 6:00");
     }
 }
