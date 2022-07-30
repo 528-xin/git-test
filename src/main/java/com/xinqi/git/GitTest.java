e239d2da5a93716e5f262c6b87fda957f48cbacb
xinqi <xinqi@qq.com>
2022-07-31 04:30:39 +0800

    first commit

---
+++ src/main/java/com/xinqi/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xinqi.git;
+
+/**
+ * @author xin麒
+ * @date 2022/7/31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git !");
+    }
+}
