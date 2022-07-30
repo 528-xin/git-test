c70dbe72f19907e20dbdf52feac6fe2f006c5cf6
xinqi <xinqi@qq.com>
2022-07-31 04:50:07 +0800

    hot-fix commit2

--- src/main/java/com/xinqi/git/GitTest.java
+++ src/main/java/com/xinqi/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello i am hot-fix branch");
+        System.out.println("hot-fix 4:49 change");
     }
 }
